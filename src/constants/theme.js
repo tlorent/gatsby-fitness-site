fea280a0a8392ca62aba8ae96930bb3b9bf7efef
tlorent <tim@oberon.nl>
2020-11-10 19:18:58 +0100

    feat: Add theme and global styling.

---
+++ src/constants/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+    colors: {
+        main: '#FC4F4F',
+        black: '#292929',
+        greyWhite: '#f5f5f5',
+        greyLight: '#CACACA',
+        greyDark: '#727272',
+        greyHover: '#EEEEEE',
+        pinkish: '#FEA9AA',
+    },
+};
+
+export default theme;
---
+++ src/globalStyling.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+const GlobalStyle = createGlobalStyle`
+    ${normalize};
+
+    html {
+        box-sizing: border-box;
+    }
+
+    *,
+    *:before,
+    *:after {
+        margin: 0;
+        padding: 0;
+        box-sizing: inherit;
+    }
+
+`;
+
+export default GlobalStyle;
